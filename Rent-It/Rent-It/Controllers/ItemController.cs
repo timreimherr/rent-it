1764ac25bb4e360f57a18d5f07db555c3adf4e07
timreimherr <timreimherr@gmail.com>
2017-04-14 21:50:03 -0500

    new project

---
+++ Rent-It/Rent-It/Controllers/ItemController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rent_It.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+    }
+}
---
+++ Rent-It/Rent-It/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rent_It.MvcApplication" Language="C#" %>
