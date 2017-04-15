65b65bc6a6acce891309dd718274fcc85c80f83c
timreimherr <timreimherr@gmail.com>
2017-04-14 22:37:57 -0500

    added customer and item pages

---
+++ Rent-It/Rent-It/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Rent_It.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rent_It.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(x => x.Id == id);
+            return View(customer);
+        }
+
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer() { Id = 1, Name = "Margret Rose" },
+                new Customer() { Id = 2, Name = "Tom Gunner" }
+            };
+
+            return customers;
+        }
+    }
+}
--- Rent-It/Rent-It/Controllers/ItemController.cs
+++ Rent-It/Rent-It/Controllers/ItemController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Rent_It.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,9 +12,23 @@ namespace Rent_It.Controllers
         // GET: Item
         public ActionResult Index()
         {
-            
-            return View();
+            var allItems = GetItems();
+            return View(allItems);
         }
 
+        public ActionResult Details(int id)
+        {
+            var item = GetItems().SingleOrDefault(x => x.Id == id);
+            return View(item);
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            return new List<Item>
+            {
+                new Item { Id = 1, Name = "Hammer" },
+                new Item { Id = 2, Name = "Flash Drive" }
+            };
+        }
     }
 }
