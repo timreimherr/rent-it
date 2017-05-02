ccc18dfa95019154d64106e1cae0c719c5b6297a
timreimherr <timreimherr@gmail.com>
2017-05-01 21:45:00 -0500

    implemented CRUD operations

--- Rent-It/Rent-It/Global.asax.cs
+++ Rent-It/Rent-It/Global.asax.cs
@@ -12,6 +12,7 @@ namespace Rent_It
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
